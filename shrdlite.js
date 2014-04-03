4902b80a45960ce9123f60a7e6fee7424a722c77
Peter Ljunglöf <peter.ljunglof@heatherleaf.se>
2014-04-03 15:07:48 +0200

    Added a "complex" example world, for testing with advanced interpreters and planners

--- shrdlite.js
+++ shrdlite.js
@@ -3,7 +3,7 @@
 var AjaxScript = "cgi-bin/ajaxwrapper.py";
 
 // List of the JSON files that contain example worlds:
-var ExampleNames = ["small","medium","impossible"];
+var ExampleNames = ["small","medium","complex","impossible"];
 var ExamplesFolder = "examples";
 
 // What the system says when it has nothing to do:
